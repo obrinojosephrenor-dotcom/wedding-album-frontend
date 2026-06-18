35dfd56e901e075210966c5d572327565f2bc2cb
Joseph Renor Obrino <obrino.josephrenor@nwssu.edu.ph>
2026-06-18 22:11:29 +0800

    Clean CSS and fix Vite import warning

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
   supabaseUrl,
-  supabaseKey
+  supabaseAnonKey
 )
